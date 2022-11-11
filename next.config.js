b3d868e15629f2b2d6fb7245d6bf6d1c8ab67864
Nadson Cerqueira <nadsoncerqueira@gmail.com>
2022-11-11 13:34:32 -0300

    versio 0.1

--- next.config.js
+++ next.config.js
@@ -2,6 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+
+  images: {
+    domains: [
+      'files.stripe.com',
+    ],
+  },
+  experimental: {
+    newNextLinkBehavior: true,
+    images: {
+      allowFutureImage: true,
+    },
+  },
 }
 
 module.exports = nextConfig
---
+++ src/pages/_app.tsx
@@ -0,0 +1,19 @@
+import { AppProps } from 'next/app'
+import { globalStyles } from '../styles/global'
+import Image from 'next/future/image'
+import { Container, Header } from '../styles/pages/app'
+import logoImg from '../assets/logo.svg'
+
+globalStyles()
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
+}
+
+
